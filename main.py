ded55aba5e6ba39a7e980ff065019b9bd3eb2c5e
AkshitaSharma211 <sharma.1608akshita@gmail.com>
2026-05-21 16:10:11 +0530

    AI chat with groq

---
+++ main.py
@@ -0,0 +1,31 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+
+messages = []
+
+print("Chat started. Type 'quit' to exit.")
+
+while True:
+    user_input = input("You: ")
+    
+    if user_input == "quit":
+        break
+    
+    messages.append({"role": "user", "content": user_input})
+    
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=messages
+    )
+    
+    reply = response.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
+    
+    print(f"AI: {reply}\n")
